6b4a255a6613d40e7e7720e5056e9e3e1f674ffc
Bryan Woods <woods.bryan@gmail.com>
2023-06-27 00:03:19 -0700

    Moving the constructor to the FromStr trait

--- src/sso/mod.rs
+++ src/sso/mod.rs
@@ -1,4 +1,5 @@
 use std::ffi::{CStr, CString, NulError};
+use std::str::FromStr;
 
 mod traits;
 pub use traits::*;
@@ -11,13 +12,26 @@ const CAPACITY: usize = ALLOCATION - 1; // for the tag byte
 
 /// A string representation that stores small strings inline,
 /// rather than in heap-allocate memory.
+///
+/// # Examples
+///
+/// You can create `Storage` from a [string slice][`std::str`] with [`Storage::from_str`]:
+///
+///
+/// ```ignore
+/// use std::str::FromStr;
+///
+/// let key = sso::Storage::from_str("blue")?;
+/// ```
 #[repr(C)]
 pub union Storage {
     bytes: [u8; ALLOCATION],
     words: [*const u8; 2],
 }
 
-impl Storage {
+impl FromStr for Storage {
+    type Err = NulError;
+
     /// # Safety
     ///
     /// This constructor sets up the preconditions that all other `unsafe` code
@@ -25,25 +39,25 @@ impl Storage {
     ///
     /// # Errors
     ///
-    /// If the `str` parameter contains a `NUL` character [`NulError`] is returned.
+    /// If the `str` parameter contains a NUL character [`NulError`] is returned.
     ///
-    pub fn from_str(str: &str) -> Result<Storage, NulError> {
+    fn from_str(s: &str) -> Result<Self, NulError> {
         let mut new = Self::default();
-        let len = str.len();
+        let len = s.len();
 
         unsafe {
             if len > CAPACITY {
-                let ptr = (CString::new(str)?.into_raw()) as *const u8;
+                let ptr = (CString::new(s)?.into_raw()) as *const u8;
                 new.bytes[0] = ALLOCATION as u8;
                 new.words[1] = ptr;
             } else {
-                if str.contains('\0') {
-                    CString::new(str)?; // returns NulError
+                if s.contains('\0') {
+                    CString::new(s)?; // returns NulError
                 }
 
                 new.bytes[0] = len as u8;
                 std::ptr::copy_nonoverlapping(
-                    str.as_bytes().as_ptr(),
+                    s.as_bytes().as_ptr(),
                     new.bytes[1..].as_mut_ptr(),
                     len,
                 );
@@ -52,7 +66,24 @@ impl Storage {
 
         Ok(new)
     }
+}
 
+impl Drop for Storage {
+    /// # Safety
+    ///
+    /// The [`Storage`] must have been created with [`Storage::from_str()`]
+    /// to guarantee that is is correctly tagged as `is_heap` or `is_inline`.    
+    ///
+    fn drop(&mut self) {
+        unsafe {
+            if self.is_heap() {
+                let _ = CString::from_raw(self.words[1] as *mut i8);
+            }
+        }
+    }
+}
+
+impl Storage {
     /// # Safety
     ///
     /// The [`Storage`] must have been created with [`Storage::from_str()`]
@@ -80,20 +111,5 @@ impl Storage {
     /// Returns whether elements are held inline.
     pub fn is_inline(&self) -> bool {
         self.is_heap() == false
-    }
-}
-
-impl Drop for Storage {
-    /// # Safety
-    ///
-    /// The [`Storage`] must have been created with [`Storage::from_str()`]
-    /// to guarantee that is is correctly tagged as `is_heap` or `is_inline`.    
-    ///
-    fn drop(&mut self) {
-        unsafe {
-            if self.is_heap() {
-                let _ = CString::from_raw(self.words[1] as *mut i8);
-            }
-        }
     }
 }
